7495f1774049baafc777c4f86b85315d055a4691
Kirill <kera.kirill@gmail.com>
2022-11-16 10:17:46 +0300

    Decline order

--- Sber.ApiClient/SberApiClient.cs
+++ Sber.ApiClient/SberApiClient.cs
@@ -10,14 +10,56 @@ namespace Sber.ApiClient
 {
     public interface ISberApiClient
     {
+        /// <summary>
+        /// Оплата заказа
+        /// </summary>
+        /// <param name="request">Данные заказа</param>
+        /// <returns></returns>
         Task<Order> RegisterPay(PayRequest request);
+        /// <summary>
+        /// Статус заказа
+        /// </summary>
+        /// <param name="request">номер заказа в системе магазина</param>
+        /// <returns></returns>
         Task<OrderStatus> GetStatus(OrderStatusRequest request);
+        /// <summary>
+        /// Проверка оплачен ли заказ
+        /// </summary>
+        /// <param name="orderNumber">номер заказа в системе магазина</param>
+        /// <returns></returns>
         Task<bool> IsOrderPaid(string orderNumber);
+        /// <summary>
+        /// Запрос возврата на полную сумму в деньгах
+        /// </summary>
+        /// <param name="request">id заказа в системе экваэринга, сумма возврата</param>
+        /// <returns></returns>
         Task<ResponseCode> Refund(RefundRequest request);
+        /// <summary>
+        /// Запрос возврата на полную сумму в деньгах
+        /// </summary>
+        /// <param name="orderNumber">номер заказа в системе магазина</param>
+        /// <param name="amount">сумма возврата</param>
+        /// <returns></returns>
         Task<ResponseCode> Refund(string orderNumber, long amount);
+        /// <summary>
+        /// Запрос отмены оплаты заказа
+        /// </summary>
+        /// <param name="request">id заказа в системе экваэринга, сумма возврата</param>
+        /// <returns></returns>
         Task<ResponseCode> Reverse(ReverseRequest request);
+        /// <summary>
+        /// Запрос отмены оплаты заказа
+        /// </summary>
+        /// <param name="orderNumber">номер заказа в системе магазина</param>
+        /// <param name="amount">сумма возврата</param>
+        /// <returns></returns>
         Task<ResponseCode> Reverse(string orderNumber, long amount);
-        Task<ResponseCode> DeclineOrder(DeclineOrderRequest request);
+        /// <summary>
+        /// Запрос отмены неоплаченного заказа
+        /// </summary>
+        /// <param name="request">номер заказа в системе магазина</param>
+        /// <returns></returns>
+        Task<ResponseCode> Decline(DeclineOrderRequest request);
     }
     public class SberApiClient : ISberApiClient
     {
@@ -99,7 +141,7 @@ namespace Sber.ApiClient
             return result;
         }
 
-        public async Task<ResponseCode> DeclineOrder(DeclineOrderRequest request)
+        public async Task<ResponseCode> Decline(DeclineOrderRequest request)
         {
             var parameters = request.ToKeyValuePair(
                 new[]{new KeyValuePair<string, string>("userName", login),
